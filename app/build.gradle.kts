84e4d3f79577b4cc2b2e57aed5b96d47e7e08ce7
Hasan <hasanboronov0523@gmail.com>
2023-10-11 07:30:12 +0500

    1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,7 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
+    implementation("de.hdodenhof:circleimageview:3.1.0")
     implementation("io.coil-kt:coil:2.4.0")
     implementation ("com.google.code.gson:gson:2.10.1")
 }
